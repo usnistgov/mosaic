be62aae896a63c22326d9e03241ead54d77403c6
Arvind Balijepalli <arvind@nist.gov>
2014-05-22 17:52:19 -0400

    Test dcOffset option in metaTrajIO

--- bin/analysis.py
+++ bin/analysis.py
@@ -103,17 +103,17 @@ def analysisiter(dir, filt, i):
 #/Volumes/DATA/SBSTags/d6TPCy3T25/20130930/p120mV6
 
 # '/Volumes/DATA/SBSTags/dA6TP30odd/20130925/p120mV'
-pyeventanalysis.SingleChannelAnalysis.SingleChannelAnalysis(
-			qdfTrajIO(dirname='/Users/arvind/Research/Experiments/PEG29EBSRefData/20120323/singleChan/' ,filter='*.qdf', nfiles=50, Rfb=9.1E+9, Cfb=1.07E-12),
-			es.eventSegment,
-			sra.stepResponseAnalysis
-		).Run() 
-
 # pyeventanalysis.SingleChannelAnalysis.SingleChannelAnalysis(
-# 			qdfTrajIO(dirname='/Volumes/DATA/polypeptide standard/20140521/p120mV2/' ,filter='*.qdf', nfiles=400, Rfb=2.11E+9, Cfb=1.16E-12),
+# 			qdfTrajIO(dirname='/Users/arvind/Research/Experiments/PEG29EBSRefData/20120323/singleChan/' ,filter='*.qdf', nfiles=50, Rfb=9.1E+9, Cfb=1.07E-12),
 # 			es.eventSegment,
 # 			sra.stepResponseAnalysis
 # 		).Run() 
+
+pyeventanalysis.SingleChannelAnalysis.SingleChannelAnalysis(
+			qdfTrajIO(dirname='/Volumes/DATA/polypeptide standard/20140521/p120mV5/' ,filter='*.qdf', dcOffset=75, Rfb=2.11E+9, Cfb=1.16E-12),
+			es.eventSegment,
+			sra.stepResponseAnalysis
+		).Run() 
 
 
 
