e9a45e8fc5fc4e28f8caae36a114897777c97e88
abalijepalli <arvind.balijepalli@nist.gov>
2014-11-24 22:17:06 -0500

    Updated and generalized MosaicAnalysis interface.

--- addons/mathematica/MosaicAnalysis.m
+++ addons/mathematica/MosaicAnalysis.m
@@ -3,15 +3,27 @@
 <<Util`
 
 
-CaptureRate[absstart_,stime_,etime_,nbins_]:=Module[{at=ArrivalTimes[absstart],a,\[Lambda],t,ft,h1},
+ScaledSingleExponentialFit[hist_, \[Lambda]_,\[Lambda]0_]:=Module[{a,t,ft},
+Return[NonlinearModelFit[Transpose[{hist[[All,1]],hist[[All,2]]/First[hist[[All,2]]]}],a Exp[-(t/\[Lambda])],{{a,1},{\[Lambda],\[Lambda]0}},t]]
+]
+
+
+PlotScaledSingleExponentialFit[hist_, ftfunc_, plotopts_]:=Module[{t,xrng={First[hist[[All,1]]],Last[hist[[All,1]]]}},
+Return[Show[{
+ListLogPlot[Transpose[{hist[[All,1]],hist[[All,2]]/hist[[All,2]][[1]]}],PlotRange->{xrng,All},PlotMarkers->Automatic,plotopts],
+LogPlot[Evaluate[ftfunc[t]],Evaluate[Flatten[{t,xrng}]],plotopts]
+}]]
+]
+
+
+CaptureRate[absstart_,stime_,etime_,nbins_,plotopts_:{}]:=Module[{at=ArrivalTimes[absstart],a,\[Lambda],t,ft,h1},
 
 h1=Hist1D[at,{stime,etime},(etime-stime)/nbins];
-ft=NonlinearModelFit[Transpose[{h1[[All,1]],h1[[All,2]]/h1[[All,2]][[1]]}],a Exp[-(t/\[Lambda])],{{a,1},{\[Lambda],Mean[at]}},t];
-Return[{{1/\[Lambda],1/(\[Lambda] Sqrt[Length[at]])}/.ft["BestFitParameters"],
-Show[{
-ListLogPlot[Transpose[{h1[[All,1]],h1[[All,2]]/h1[[All,2]][[1]]}],PlotRange->{{stime,etime},All},PlotMarkers->Automatic,Epilog->{Text[NumberForm[1/\[Lambda],{4,0}]\[PlusMinus]NumberForm[1/(\[Lambda] Sqrt[Length[at]]),{2,1}]/.ft["BestFitParameters"],{(etime-stime)/2,Log[0.5]}]}],
-LogPlot[Evaluate[Normal[ft]],{t,stime,etime},PlotStyle->Thick]
-},ImageSize->300]
+ft=ScaledSingleExponentialFit[h1,\[Lambda],Mean[at]];
+Return[{
+{1/\[Lambda],1/(\[Lambda] Sqrt[Length[at]])}/.ft["BestFitParameters"],PlotScaledSingleExponentialFit[h1, ft["Function"],plotopts],
+h1,
+ft["Function"]
 }]
 ]
 
@@ -19,4 +31,5 @@ LogPlot[Evaluate[Normal[ft]],{t,stime,etime},PlotStyle->Thick]
 ArrivalTimes[arrtime_]:=Flatten[Differences/@Partition[arrtime,2,1]]/1000
 
 
-kon[cap_,c_]:=1/((1/cap)c)
+(* ::Text:: *)
+(*kon[cap_, c_] := 1/((1/cap) c)*)
