15091490e8e90c50d2ede466c04cf1309b52541b
abalijepalli <arvind.balijepalli@nist.gov>
2016-02-09 16:34:43 -0500

    Updated build number logging.

--- mosaic/_version.py
+++ mosaic/_version.py
@@ -1,7 +1,17 @@
 import subprocess
 
 __version__="1.3"
+
 try:
 	__build__=subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
 except:
 	__build__=""
+
+try:
+	if not __build__:
+		with open('commit-hash', 'r') as f:
+			__build__=f.read().strip()
+except:
+	__build__=""
+
+
