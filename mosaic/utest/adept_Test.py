fabc6330e288e1f4022868ac648c910c1189f97f
abalijepalli <arvind.balijepalli@nist.gov>
2016-05-13 20:42:36 -0400

    Refactor unit tests.

---
+++ mosaic/utest/adept_Test.py
@@ -0,0 +1,8 @@
+import mosaic.adept as adept
+from mosaic.utest.algorithmCommon import BaseMultiStateTest
+
+class ADEPT_TestSuite(BaseMultiStateTest):
+	def test_adept(self):
+		for i in [0,2,3,4,5,6,7,8,10,11,12,14,15,16,17,18,19,20]:
+			basename='testdata/eventLong_'+str(i)
+			yield self.runTestCase, basename+'_raw.bin', basename+'_params.json', adept.adept
---
+++ mosaic/utest/import_Tests.py
@@ -0,0 +1,71 @@
+class ModuleImportTest(object):
+	def runTestCase(self, modulename):
+		module=__import__(modulename)
+
+		for submod in modulename.split('.')[1:]:
+			module=getattr(module, submod)
+
+		return
+		
+class ModuleImport_TestSuite(ModuleImportTest):
+	def test_moduleImport(self):
+		moduleList=[
+		'mosaic',
+		'mosaic.ConvertToCSV', 
+		'mosaic.besselLowpassFilter',	
+		'mosaic.metaEventPartition', 
+		'mosaic.settings',
+		'mosaic.SingleChannelAnalysis',	
+		'mosaic.binTrajIO', 
+		'mosaic.metaEventProcessor', 
+		'mosaic.singleStepEvent', 
+		'mosaic.commonExceptions', 
+		'mosaic.metaIOFilter', 
+		'mosaic.sqlite3MDIO',
+		'mosaic._version', 
+		'mosaic.convolutionFilter',
+		'mosaic.metaMDIO', 
+		'mosaic.tsvTrajIO',
+		'mosaic.abfTrajIO', 
+		'mosaic.cusumPlus', 
+		'mosaic.metaTrajIO', 
+		'mosaic.waveletDenoiseFilter',
+		'mosaic.adept', 
+		'mosaic.errors', 
+		'mosaic.pproc', 
+		'mosaic.zmqIO',
+		'mosaic.adept2State',
+		'mosaic.eventSegment', 
+		'mosaic.qdfTrajIO', 
+		'mosaic.zmqWorker',
+		'mosaic.utilities.ionic_current_stats', 
+		'mosaic.utilities.resource_path',
+		'mosaic.utilities.analysis',
+		'mosaic.utilities.mosaicLog',
+		'mosaic.utilities.sqlQuery',
+		'mosaic.utilities.fit_funcs',
+		'mosaic.utilities.mosaicTiming',
+		'mosaic.utilities.util',
+		'mosaicgui',
+		'mosaicgui.EBSStateFileDict',
+		'mosaicgui.autocompleteedit',
+		'mosaicgui.mosaicGUI',
+		'mosaicgui.sqlQueryWorker',
+		'mosaicgui.datamodel', 
+		'mosaicgui.mplwidget',
+		'mosaicgui.updateService',
+		'mosaicgui.analysisWorker',
+		'mosaicgui.datapathedit',
+		'mosaicgui.settingsview',
+		'mosaicgui.aboutdialog.aboutdialog',
+		'mosaicgui.advancedsettings.advancedsettings',
+		'mosaicgui.blockdepthview.blockdepthview',
+		'mosaicgui.consolelog.consolelog',
+		'mosaicgui.csvexportview.csvexportview',
+		'mosaicgui.fiteventsview.fiteventsview',
+		'mosaicgui.statisticsview.statisticsview',
+		'mosaicgui.trajview.trajview'
+		]
+
+		for module in moduleList:
+			yield self.runTestCase, module
