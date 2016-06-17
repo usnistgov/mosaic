39855fda8d34b270343bea4bc03a5e71b1a5b175
abalijepalli <arvind.balijepalli@nist.gov>
2016-06-17 12:13:09 -0400

    Added new developer options to control log output.

--- mosaic/_global.py
+++ mosaic/_global.py
@@ -1,3 +1,6 @@
 # Control global settings
-DeveloperMode=False
+DeveloperMode=False		# Turn on developer options below
+
+CodeProfiling='summary'	# Either 'summary' to print a summary at the end of a run, 'none' for not timing, or 'all' to print timing for every function call
+LogProperties=True 		# Log all class properties defined with mosaic_property
 LogSizeBytes=int(2<<20) # 2 MB
