5c158dd4a40601fefa0fc896e77556225f607972
abalijepalli <arvind.balijepalli@nist.gov>
2016-06-17 13:47:01 -0400

    Update global settings.

--- mosaic/_global.py
+++ mosaic/_global.py
@@ -1,6 +1,8 @@
 # Control global settings
-DeveloperMode=False		# Turn on developer options below
+DeveloperMode=False		# Turn on developer options.
 
-CodeProfiling='summary'	# Either 'summary' to print a summary at the end of a run, 'none' for not timing, or 'all' to print timing for every function call
-LogProperties=True 		# Log all class properties defined with mosaic_property
+CodeProfiling='summary'	# Either 'summary' to print a summary at the end of a run, 
+						# 'none' for not timing, or 
+						# 'all' to print timing of every function call profiled.
+LogProperties=False		# Log all class properties defined with mosaic_property.
 LogSizeBytes=int(2<<20) # 2 MB
