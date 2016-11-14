f0e754c6b66c46c4463ffde179b6207a516f7dd7
abalijepalli <arvind.balijepalli@nist.gov>
2016-11-14 11:52:18 -0500

    Version 1.3

--- mosaic/_version.py
+++ mosaic/_version.py
@@ -1,7 +1,7 @@
 import subprocess
 from mosaic.utilities.resource_path import resource_path
 
-__version__="1.3b4"
+__version__="1.3"
 
 try:
 	__build__=subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], stderr=subprocess.STDOUT).strip()
