3e53b59c9c9616c55935ac1d8e7a7f4cc4b030a9
abalijepalli <arvind.balijepalli@nist.gov>
2016-11-16 13:10:31 -0500

    Version 1.3

--- download.py
+++ download.py
@@ -118,7 +118,7 @@ def GenerateTableFromVersion(baseurl, title, version, showBinaries=True):
 if __name__ == '__main__':
   url="https://github.com/usnistgov/mosaic/releases/download/v"
   vers=[
-    ("Version "+str(mosaic.__version__), str(mosaic.__version__)+'+f0e754c', True),
+    ("Version "+str(mosaic.__version__), str(mosaic.__version__)+'.f0e754c', True),
     ("Version 1.2", "1.2",False),
     ("Version 1.1", "1.1",False),
     ("Version 1.0", "1.0",False)
