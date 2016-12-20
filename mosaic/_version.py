04e4ad48f7e966698990a937620b8b1a1e4435af
abalijepalli <arvind.balijepalli@nist.gov>
2016-12-20 11:14:57 -0500

    Update version reporting.

--- mosaic/_version.py
+++ mosaic/_version.py
@@ -2,6 +2,17 @@ import subprocess
 from mosaic.utilities.resource_path import resource_path
 
 __version__="1.3.1"
+try:
+	__version__=subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags'], stderr=subprocess.STDOUT).strip().lstrip('v')
+except:
+	__version__=""
+
+try:
+	if not __version__:
+		with open( resource_path('version-hash'), 'r' ) as f:
+			__version__=f.read().strip()		
+except:
+	__version__=""
 
 try:
 	__build__=subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], stderr=subprocess.STDOUT).strip()
