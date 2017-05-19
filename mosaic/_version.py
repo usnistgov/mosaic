8d4771f6aabb560301d80e93769d75e992d99abb
abalijepalli <arvind.balijepalli@nist.gov>
2017-05-19 16:16:16 -0400

    Include MOSAIC web versioning within the main package.

--- mosaic/_version.py
+++ mosaic/_version.py
@@ -2,7 +2,7 @@ import subprocess
 from mosaic.utilities.resource_path import resource_path
 
 try:
-	__version__=subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags'], stderr=subprocess.STDOUT).strip().lstrip('v')
+	__version__=subprocess.check_output(['git', 'describe', '--abbrev=0', '--match', 'v*', 'HEAD'], stderr=subprocess.STDOUT).strip().lstrip('v')
 except:
 	__version__=""
 
@@ -12,6 +12,18 @@ try:
 			__version__=f.read().strip()		
 except:
 	__version__=""
+
+try:
+	__mweb_version__=subprocess.check_output(['git', 'describe', '--abbrev=0', '--match', 'mwebv*', 'HEAD'], stderr=subprocess.STDOUT).strip().lstrip('mwebv')
+except:
+	__mweb_version__=""
+
+try:
+	if not __mweb_version__:
+		with open( resource_path('mweb-version-hash'), 'r' ) as f:
+			__mweb_version__=f.read().strip()		
+except:
+	__mweb_version__=""
 
 try:
 	__build__=subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], stderr=subprocess.STDOUT).strip()
