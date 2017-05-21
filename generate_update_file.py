e32daeddea4c9ef046f1c7e5a78c32e6c144474d
abalijepalli <arvind.balijepalli@nist.gov>
2017-05-20 21:21:55 -0400

    Rollback to 1.3.2

--- generate_update_file.py
+++ generate_update_file.py
@@ -33,7 +33,7 @@ def updateVers(verlist):
 updatejson="""{
 		"version" 			: '"""+base64.b64encode(str(version))+"""',
 		"build"				: '"""+base64.b64encode(bld)+"""',
-		"update-versions"	: '"""+updateVers("""["1.0", "1.1", "1.2", "1.3b1", "1.3b2", "1.3b3", "1.3", "1.3.1", "1.3.2"]""")+"""', 
+		"update-versions"	: '"""+updateVers("""["1.0", "1.1", "1.2", "1.3b1", "1.3b2", "1.3b3", "1.3", "1.3.1"]""")+"""', 
 		"changelog"			: '"""+readfile('../CHANGELOG.rst')+"""',
 		"dl-w64"			: '"""+dl_win("https://github.com/usnistgov/mosaic/releases/download/v")+"""',	
 		"dl-osx"			: '"""+dl_osx("https://github.com/usnistgov/mosaic/releases/download/v")+"""'
