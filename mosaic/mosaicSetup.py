008a733e193f84897d213e506f13b2e370235d4d
abalijepalli <arvind.balijepalli@nist.gov>
2017-10-07 22:37:39 -0400

    More runMOSAIC script refactoring.

---
+++ mosaic/mosaicSetup.py
@@ -0,0 +1,30 @@
+import sys
+import multiprocessing
+import argparse
+
+from mosaicweb.run import startMOSAICWeb
+from mosaicgui.run import startMOSAICQt
+
+
+class mosaicSetup:
+	def __init__(self):
+		self.parseCLIArgs()
+
+	def parseCLIArgs(self):
+		self.parser = argparse.ArgumentParser(description='Run the MOSAIC graphical interface')
+		
+		self.parser.add_argument('-w', '--web', dest='web', default=False, action='store_true', help='Run the MOSAIC web interface')
+		self.parser.add_argument('-q', '--qt', dest='qt', default=False, action='store_true', help='Run the MOSAIC Qt interface')
+		
+		self.args = vars(self.parser.parse_args())
+
+	def launcMOSAIC(self):
+		if sys.platform.startswith('win'):
+			multiprocessing.freeze_support()
+
+		if self.args["qt"]:
+			startMOSAICQt()
+		elif self.args["web"]:
+			startMOSAICWeb()
+		else:
+			self.parser.print_help()
