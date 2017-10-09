64c331e2e31e776486a4f50352e5122050340bb0
abalijepalli <arvind.balijepalli@nist.gov>
2017-10-09 14:03:43 -0400

    Version 1.3.5

--- download.py
+++ download.py
@@ -122,7 +122,8 @@ if __name__ == '__main__':
 
   url="https://github.com/usnistgov/mosaic/releases/download/v"
   vers=[
-    ("Version "+str(version), str(version), '.4932336', True),
+    ("Version "+str(version), str(version), '.801e7e5', True),
+    ("Version 1.3.4", "1.3.4", '.4932336', False),
     ("Version 1.3.2", "1.3.2", ".d88866c", False),
     ("Version 1.3.1", "1.3.1", ".9977009", False),
     ("Version 1.3", "1.3", ".f0e754c", False),
--- generate_update_file.py
+++ generate_update_file.py
@@ -4,7 +4,7 @@ import base64
 
 with open( resource_path('version-hash'), 'r' ) as f:
       version=f.read().strip()
-bld='4932336'
+bld='801e7e5'
 
 def enc(func):
 	def f(s):
@@ -13,12 +13,14 @@ def enc(func):
 
 @enc
 def dl_osx(baseurl):
+	print "dl-osx: "+"{0}{1}/mosaic-{1}.{2}.dmg".format(baseurl, version, bld)
 	return "{0}{1}/mosaic-{1}.{2}.dmg".format(baseurl, version, bld)
 	# return "{0}1.2/mosaic-1.2.dmg".format(baseurl)
 
 
 @enc
 def dl_win(baseurl):
+	print "dl-win: "+"{0}{1}/mosaic-x64-{1}.{2}.zip".format(baseurl, version, bld)
 	return "{0}{1}/mosaic-x64-{1}.{2}.zip".format(baseurl, version, bld)
 	# return "{0}1.2/mosaic-x64-1.2.zip".format(baseurl)
 
@@ -28,16 +30,17 @@ def readfile(fname):
 		return f.read()
 @enc
 def updateVers(verlist):
+	print "update-versions: "+verlist
 	return verlist
 
 updatejson="""{
 		"version" 			: '"""+base64.b64encode(str(version))+"""',
 		"build"				: '"""+base64.b64encode(bld)+"""',
-		"update-versions"	: '"""+updateVers("""["1.0", "1.1", "1.2", "1.3b1", "1.3b2", "1.3b3", "1.3", "1.3.1", "1.3.2", "1.3.3"]""")+"""', 
+		"update-versions"	: '"""+updateVers("""["1.0", "1.1", "1.2", "1.3b1", "1.3b2", "1.3b3", "1.3", "1.3.1", "1.3.2", "1.3.3","1.3.4"]""")+"""', 
 		"changelog"			: '"""+readfile('../CHANGELOG.rst')+"""',
 		"dl-w64"			: '"""+dl_win("https://github.com/usnistgov/mosaic/releases/download/v")+"""',	
 		"dl-osx"			: '"""+dl_osx("https://github.com/usnistgov/mosaic/releases/download/v")+"""'
 	}"""
 
 with open('version.json', 'w') as f:
-	 f.write( base64.b64encode( updatejson.replace("'",'"') ) )
+	f.write( base64.b64encode( updatejson.replace("'",'"') ) )
