c4170b6418f86150cbd50d3008a98dd583e2427b
abalijepalli <arvind.balijepalli@nist.gov>
2018-02-14 06:57:35 -0500

    Temporarily disable Qt interface through runMosaic script.

--- mosaic/mosaicSetup.py
+++ mosaic/mosaicSetup.py
@@ -15,13 +15,14 @@ import argparse
 
 class mosaicSetup:
 	def __init__(self):
-		self.parseCLIArgs()
+		# self.parseCLIArgs()
+		pass
 
 	def parseCLIArgs(self):
 		self.parser = argparse.ArgumentParser(description='Run the MOSAIC graphical interface')
 		
-		self.parser.add_argument('-w', '--web', dest='web', default=False, action='store_true', help='Run the MOSAIC web interface (default)')
-		self.parser.add_argument('-q', '--qt', dest='qt', default=True, action='store_true', help='Run the MOSAIC Qt interface')
+		self.parser.add_argument('-w', '--web', dest='web', default=True, action='store_true', help='Run the MOSAIC web interface (default)')
+		self.parser.add_argument('-q', '--qt', dest='qt', default=False, action='store_true', help='Run the MOSAIC Qt interface')
 		
 		self.args = vars(self.parser.parse_args())
 
@@ -29,21 +30,23 @@ class mosaicSetup:
 		if sys.platform.startswith('win'):
 			multiprocessing.freeze_support()
 
-		if self.args["web"]:
-			try:
-				from mosaicweb.run import startMOSAICWeb
-			except ImportError, err:
-				print "Missing dependencies for Web GUI ({0}).".format(err)
-				return
+		# if self.args["qt"]:
+		# 	try:
+		# 		from mosaicgui.run import startMOSAICQt
+		# 	except ImportError, err:
+		# 		print "Missing dependencies for Qt GUI ({0}).".format(err)
+		# 		return
 
-			startMOSAICWeb()
-		elif self.args["qt"]:
-			try:
-				from mosaicgui.run import startMOSAICQt
-			except ImportError, err:
-				print "Missing dependencies for Qt GUI ({0}).".format(err)
-				return
-
-			startMOSAICQt()
-		else:
-			self.parser.print_help()
+		# 	startMOSAICQt()
+		# elif self.args["web"]:
+		# 	try:
+		# 		from mosaicweb.run import startMOSAICWeb
+		# 	except ImportError, err:
+		# 		print "Missing dependencies for Web GUI ({0}).".format(err)
+		# 		return
+		#	startMOSAICWeb()
+		# else:
+		# 	self.parser.print_help()
+		from mosaicweb.run import startMOSAICWeb
+		startMOSAICWeb()
+	
