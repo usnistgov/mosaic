c96187707a743e4b6fba36274adde405560e3bed
abalijepalli <arvind.balijepalli@nist.gov>
2018-06-07 09:18:21 -0400

    Added several trajio tests.

--- mosaic/tests/trajio_Test.py
+++ mosaic/tests/trajio_Test.py
@@ -1,3 +1,6 @@
+import copy
+from nose.tools import raises
+import mosaic.trajio.metaTrajIO as metaTrajIO
 import mosaic.trajio.qdfTrajIO as qdfTrajIO
 import mosaic.trajio.binTrajIO as binTrajIO
 import mosaic.trajio.tsvTrajIO as tsvTrajIO
@@ -7,7 +10,11 @@ import numpy as np
 class TrajIOTest(object):
 	_refcurrent=[135.99604311732614, 6.6679749349455024]
 	_trajioHnd={
-		'qdf' : ( qdfTrajIO.qdfTrajIO, {'filter' : 'SingleChan-0001.qdf', 'Rfb' : 9.1e9, 'Cfb' : 1.07e-12} ),
+		'qdf' : ( qdfTrajIO.qdfTrajIO, {
+						'filter' : 'SingleChan-0001.qdf', 
+						'Rfb' : 9.1e9, 
+						'Cfb' : 1.07e-12
+					} ),
 		'bin' : ( binTrajIO.binTrajIO, {
 						"AmplifierScale": "1.0", 
 						"AmplifierOffset": 0.0, 
@@ -17,7 +24,7 @@ class TrajIOTest(object):
 						"IonicCurrentColumn" : "curr_pA",
 						"dcOffset": 0.0, 
 						"filter": "SingleChan-0001_1.bin"
-						} 
+					} 
 				),	
 		'tsv' : ( tsvTrajIO.tsvTrajIO, {
 				        "filter" :  "SingleChan-0001_1.tsv", 
@@ -46,14 +53,79 @@ class TrajIOTest(object):
 
 		dat=q.popdata(100000)
 		current=self.currentStats(dat)
+		q._formatsettings()
 
 		assert round(current[0],1)==round(TrajIOTest._refcurrent[0],1)
 		assert round(current[1],1)==round(TrajIOTest._refcurrent[1],1)
+
+	@raises(metaTrajIO.InsufficientArgumentsError, KeyError)
+	def runSettingsErrorTestCase(self,dattype, dirname, setting):
+		t=copy.deepcopy(TrajIOTest._trajioHnd[dattype])
+
+		t[1].pop(setting)
+
+		t[0](dirname=dirname, **t[1])
+
+	def runBINErrorTestCase(self, dirname, setting):
+		self.runSettingsErrorTestCase('bin', dirname, setting)
+
+	def runBINSettingsTestCase(self, dirname, setting, val):
+		t=copy.deepcopy(TrajIOTest._trajioHnd['bin'])
+
+		t[1].pop(setting)
+
+		q=t[0](dirname=dirname, **t[1])
+
+		assert getattr(q, setting)==val
+
+	def runQDFErrorTestCase(self, dirname, setting):
+		self.runSettingsErrorTestCase('qdf', dirname, setting)
+
+	def runQDFCurrentTestCase(self, dirname):
+		t=TrajIOTest._trajioHnd['qdf']
+
+		d=copy.deepcopy(t[1])
+		d['format']='i'
+	
+		q=t[0](dirname=dirname, **d)
+
+		dat=q.popdata(100000)
+
+		assert len(dat)>0
+
+	@raises(IndexError)
+	def runTSVCurrentTestCase(self, dirname):
+		t=TrajIOTest._trajioHnd['tsv']
+
+		d=copy.deepcopy(t[1])
+		d.pop("Fs")
+	
+		q=t[0](dirname=dirname, **d)
+
+		dat=q.popdata(1000)
 
 class TrajIO_TestSuite(TrajIOTest):
 	def test_trajio(self):
 		for dat in ['qdf','bin', 'tsv', 'csv']:
 			yield self.runTestCase, dat, 'mosaic/tests/testdata/'
+
+	def test_binErrorTest(self):
+		for k in ["SamplingFrequency", "ColumnTypes", "IonicCurrentColumn"]:
+			yield self.runBINErrorTestCase, 'mosaic/tests/testdata/', k
+
+	def test_binSettingsTest(self):
+		for k in [("HeaderOffset", 0), ("AmplifierScale",1), ("AmplifierOffset",0)]:
+			yield self.runBINSettingsTestCase, 'mosaic/tests/testdata/', k[0], k[1]
+
+	def test_qdfErrorTest(self):
+		for k in ["Rfb", "Rfb"]:
+			yield self.runQDFErrorTestCase, 'mosaic/tests/testdata/', k
+
+	def test_qdfCurrentTest(self):
+		yield self.runQDFCurrentTestCase, 'mosaic/tests/testdata/'
+
+	def test_tsvSettingsTest(self):
+		yield self.runTSVCurrentTestCase, 'mosaic/tests/testdata/'
 
 if __name__ == '__main__':
 	t=TrajIOTest()
