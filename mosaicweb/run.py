0b05f1b777eddd269495d7c1e1ab8881fc06bce3
Arvind Balijepalli <arvind.balijepalli@nist.gov>
2021-04-14 19:47:30 -0400

    Bug fixes

--- mosaicweb/run.py
+++ mosaicweb/run.py
@@ -1,25 +1,28 @@
 import webbrowser
 import sys
-import gunicorn.app.base
 import mosaicweb
 from mosaic.utilities.ga import registerLaunch
 import mosaic
 
-class mosaicApplication(gunicorn.app.base.BaseApplication):
 
-	def __init__(self, app, options=None):
-		self.options = options or {}
-		self.application = app
-		super().__init__()
+if not sys.platform.startswith('win'):
+	import gunicorn.app.base
 
-	def load_config(self):
-		config = {key: value for key, value in self.options.items()
-				if key in self.cfg.settings and value is not None}
-		for key, value in config.items():
-			self.cfg.set(key.lower(), value)
+	class mosaicApplication(gunicorn.app.base.BaseApplication):
 
-	def load(self):
-		return self.application
+		def __init__(self, app, options=None):
+			self.options = options or {}
+			self.application = app
+			super().__init__()
+
+		def load_config(self):
+			config = {key: value for key, value in self.options.items()
+					if key in self.cfg.settings and value is not None}
+			for key, value in config.items():
+				self.cfg.set(key.lower(), value)
+
+		def load(self):
+			return self.application
 
 @registerLaunch("mweb")
 def startMOSAICWeb(newWindow=True):
