23b20ee52be4cf97ccf4a80e6deba909ef86c6d6
Arvind Balijepalli <arvind.balijepalli@nist.gov>
2021-05-08 15:02:50 -0400

    Add a check to verify version entry to ensure it is a valid settings file.

--- mosaic/trajio/ChimeraSettingsDict.py
+++ mosaic/trajio/ChimeraSettingsDict.py
@@ -7,6 +7,7 @@ Parse Chimera VC100 settings file for ADC parameters.
 	:License:	See LICENSE.TXT
 	:ChangeLog:
 	.. line-block::
+		5/08/21		AB 	Add a check to verify version entry to ensure it is a valid settings file.
 		5/08/21     AB	Initial version
 """
 import csv
@@ -17,6 +18,14 @@ class ChimeraSettingsDict(dict):
 			statereader = csv.reader(csvfile, delimiter='=', quotechar='|')
 			
 			[ self.update(self.extractParam(row)) for row in statereader if len(row) > 1 ]
+
+		try:
+			if self.__version__>=1:
+				pass
+			else:
+				self.clear()
+		except:
+			self.clear()
 
 	def __getitem__(self, key):
 		try:
@@ -33,6 +42,13 @@ class ChimeraSettingsDict(dict):
 			return default
 	
 	def extractParam(self, line):
+		try:
+			if line[0]=='__version__':
+				self.__version__=float(line[1])
+				return {}
+		except:
+			return {}
+
 		d=line[0].split('_')
 		if d[0]=='SETUP':
 			if d[1]=="ADCSAMPLERATE":
@@ -41,13 +57,4 @@ class ChimeraSettingsDict(dict):
 				return {d[1]: float(line[1])}
 		else:
 			return {}
-	
-if __name__ == "__main__":
-	s=ChimeraSettingsDict("K439PC - pH 10 1M KCl G 104.5mScm - pH 8 3.6M LiCl G 164.3mScm - -200mV 31nM 750-b-p dsDNA EXP2 .txt")
-
-	print(s)
-
-	print(s["SamplingFrequency"])
-	print(s["ADCSAMPLERATE"])
-
 	
